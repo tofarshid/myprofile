716938c12592d57a72d285fce78a104472280664
Farshid Kamal <fkamal@Farshids-Laptop.lan>
2025-02-13 19:18:37 +1100

    added userprofile

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { Authenticator } from '@aws-amplify/ui-react';
+const t = document.getElementById('root') as HTMLElement;
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(t).render(
+    <React.StrictMode>
+        <Authenticator>
+            <App />
+        </Authenticator>
+    </React.StrictMode>
+);
